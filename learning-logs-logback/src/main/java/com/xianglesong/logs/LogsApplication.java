199e1498b8cf198c397db8a91f7ae995079a7e7c
马如林 <marulin@ceair.com>
2017-09-16 12:05:19 +0800

    add

--- learning-logs-logback/src/main/java/com/xianglesong/logs/LogsApplication.java
+++ learning-logs-logback/src/main/java/com/xianglesong/logs/LogsApplication.java
@@ -2,13 +2,22 @@ package com.xianglesong.logs;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 public class LogsApplication {
   private static final Logger logger = LoggerFactory.getLogger(LogsApplication.class);
 
   public static void main(String[] args) {
     System.out.println("begin.");
+    // simple
     logger.info("this is a test.");
+
+    // mdc test, example: uid
+    MDC.put("path1", "path1-test");
+    MDC.put("mdcvar2", "test2");
+    MDC.put("uid", "uid1-test");
+    logger.info("mdc-test uid-test");
+
     System.out.println("end.");
   }
 }
--- learning-logs-logback/src/main/java/com/xianglesong/logs/appender/MyAppender.java
+++ learning-logs-logback/src/main/java/com/xianglesong/logs/appender/MyAppender.java
@@ -8,20 +8,20 @@ public class MyAppender extends UnsynchronizedAppenderBase<LoggingEvent> {
   @Override
   protected void append(LoggingEvent eventObject) {
     // 自定义日志
-    System.out.println("myAppender 1: " + eventObject.getMessage() + eventObject.getLevel()
-        + eventObject.getFormattedMessage() + eventObject.getLoggerName()
+    System.out.println("myAppender 1: " + eventObject.getMessage() + " " + eventObject.getLevel()
+        + " " + eventObject.getFormattedMessage() + " " + eventObject.getLoggerName() + " "
         + eventObject.getTimeStamp());
     // System.out.println("myApperder 2: " + eventObject.getCallerData());
-    StackTraceElement[] callerDataArray = eventObject.getCallerData();
-    System.out.println("myApperder 2: " + eventObject.getCallerData());
-    int index = 0;
-    if (callerDataArray != null && callerDataArray.length > 0) {
-      System.out.println("class: " + callerDataArray[index].getClassName());
-      System.out.println("method: " + callerDataArray[index].getMethodName());
-      System.out.println("file: " + callerDataArray[index].getFileName());
-      System.out.println("line: " + callerDataArray[index].getLineNumber());
-      System.out.println();
-    }
+    // StackTraceElement[] callerDataArray = eventObject.getCallerData();
+    // System.out.println("myApperder 2: " + eventObject.getCallerData());
+    // int index = 0;
+    // if (callerDataArray != null && callerDataArray.length > 0) {
+    // System.out.println("class: " + callerDataArray[index].getClassName());
+    // System.out.println("method: " + callerDataArray[index].getMethodName());
+    // System.out.println("file: " + callerDataArray[index].getFileName());
+    // System.out.println("line: " + callerDataArray[index].getLineNumber());
+    // System.out.println();
+    // }
 
   }
 
